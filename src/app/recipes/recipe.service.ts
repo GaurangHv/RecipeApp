1725f6f09ae2d9a97dbac0213a002dbb30ee8f60
GaurangHv <omvirgaurang@gmail.com>
2017-11-24 19:00:55 +0530

    In ingredient's tomatos changed form 3 to 2

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,7 @@ export class RecipeService {
       'A super-tasty spicy Vada pav!',
       'https://mysticalcafe.in/wp-content/uploads/2016/12/Vada-Pav.png',
       [
-        new Ingredient('Tameta', 3),
+        new Ingredient('Tameta', 2),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
@@ -22,7 +22,7 @@ export class RecipeService {
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
         new Ingredient('Buns', 2),
-        new Ingredient('Tameta', 3)
+        new Ingredient('Tameta', 2)
       ])
   ];
 
